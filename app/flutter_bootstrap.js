81638eb6a216eb5ba24256c022e82c491003430c
CV Manager AI <support@cvmanagerai.com>
2025-12-04 11:44:24 +0100

    Deploy password reset fix

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1537883584"
+    serviceWorkerVersion: "2830374901"
   }
 });
