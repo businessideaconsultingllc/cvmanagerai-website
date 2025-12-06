7a0f48bf6c0f6c9e9892725f9518f4b132d94eee
CV Manager AI <support@cvmanagerai.com>
2025-12-06 12:32:29 +0100

    Update web app with Edge Function CORS fix

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1151288874"
+    serviceWorkerVersion: "3188320225"
   }
 });
