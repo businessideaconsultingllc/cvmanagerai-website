61e87962cf6dad6c26b2972e78a960bcf0ddbff0
CV Manager AI <deploy@cvmanagerai.com>
2025-12-11 08:49:50 +0100

    Fix Google Sign-In routing issues - OAuth callback detection

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "cbece2a9fceeedb03da0edb0bce0bf36
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "173fda750b21797711959973e5b0e4bb",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "0510d3082d54bd646849eacf2b67da1b",
+"flutter_bootstrap.js": "d25c673b56ebfbd89d59fd9e699c76f0",
 "icons/Icon-192.png": "acf929d00ef91b4f70c4cfc28c03b568",
 "icons/Icon-512.png": "af189429479bb6ea1c781f05340f901b",
 "icons/Icon-maskable-192.png": "acf929d00ef91b4f70c4cfc28c03b568",
 "icons/Icon-maskable-512.png": "af189429479bb6ea1c781f05340f901b",
 "index.html": "4a2aa5bbf033076797eecac9b5ee4947",
 "/": "4a2aa5bbf033076797eecac9b5ee4947",
-"main.dart.js": "ea9351c3e1d0930f67cbea62dcee84fd",
+"main.dart.js": "6142d25b245b1c4587471364c69e79d2",
 "manifest.json": "28a7bba9798a2752e838863b8e6e0c10",
 "version.json": "699673d9149f2031ce9019297deb7ca3"};
 // The application shell files that are downloaded before a service worker can
