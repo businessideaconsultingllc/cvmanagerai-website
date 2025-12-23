1c63d3464d05c197ff198f70e89aa36c05cc17bc
Deployment Script <deploy@local.script>
2025-12-23 16:02:54 +0100

    Update Flutter App (Automated Deploy)

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1561132849"
+    serviceWorkerVersion: "4109819536"
   }
 });
