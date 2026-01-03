62a75e2a4dcb3740b21f22e1d829796dba70b4bf
Deployment Script <deploy@local.script>
2026-01-03 15:41:00 +0100

    Update Flutter App (Automated Deploy)

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "80492066c03b4d758091fa05d8f91f07
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "f4ad63eae138bd29996d91317eb50cfc",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "a32c23aa0c7d91a79dadd9cbedf8643f",
+"flutter_bootstrap.js": "8151af20933c44b8a84a0baf39f34e41",
 "icons/Icon-192.png": "dbcfa054393b16b7817eec1ec5fb8caf",
 "icons/Icon-512.png": "1ba73877555e0e0ae87d426a25a27552",
 "icons/Icon-maskable-192.png": "dbcfa054393b16b7817eec1ec5fb8caf",
