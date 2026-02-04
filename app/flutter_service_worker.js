178728dcd55d78a4f983f92cfd0243ccb4ee3933
Deployment Script <deploy@local.script>
2026-02-04 16:31:21 +0100

    Major mobile UX fix: refactored edit dialogs to StatefulWidgets for cursor stability

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "80492066c03b4d758091fa05d8f91f07
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "f4ad63eae138bd29996d91317eb50cfc",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "dfb403c3f1cc32b4498527baec5b4590",
+"flutter_bootstrap.js": "a83eceedfb7ab0ff1768947c347caa2e",
 "icons/Icon-192.png": "dbcfa054393b16b7817eec1ec5fb8caf",
 "icons/Icon-512.png": "1ba73877555e0e0ae87d426a25a27552",
 "icons/Icon-maskable-192.png": "dbcfa054393b16b7817eec1ec5fb8caf",
 "icons/Icon-maskable-512.png": "1ba73877555e0e0ae87d426a25a27552",
 "index.html": "c494e76216197ed1ee22c1573c4b0699",
 "/": "c494e76216197ed1ee22c1573c4b0699",
-"main.dart.js": "01c3d3334b9cf0d458daa12598c14b31",
+"main.dart.js": "e2a11c2b05e3c1f239bc1d6b73b478a1",
 "manifest.json": "4a2198c0ba6cab70502dac0a601d9f39",
 "version.json": "699673d9149f2031ce9019297deb7ca3"};
 // The application shell files that are downloaded before a service worker can
