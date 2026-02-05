95cdfb7add778df48e600c900acf40ab320dca96
Deployment Script <deploy@local.script>
2026-02-05 11:19:25 +0100

    Ultimate mobile UX fix: GlobalKey anchoring and rebuild isolation for perfect cursor stability

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1902437840"
+    serviceWorkerVersion: "1380626749"
   }
 });
