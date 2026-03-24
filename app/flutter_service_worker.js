41f8750a21ddc7e0a2071e18557b8426ebcd9383
Deployment Script <deploy@local.script>
2026-03-24 16:19:43 +0100

    Fix: CV UI scrolling/selection, Gradle JVM crash, and Keyboard scrolling conflict

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "80492066c03b4d758091fa05d8f91f07
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "f4ad63eae138bd29996d91317eb50cfc",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "05dd7ff876f11ccf046aa096ad7bb597",
+"flutter_bootstrap.js": "3ebd99f0cfd457b3e50ce1667502e36b",
 "icons/Icon-192.png": "dbcfa054393b16b7817eec1ec5fb8caf",
 "icons/Icon-512.png": "1ba73877555e0e0ae87d426a25a27552",
 "icons/Icon-maskable-192.png": "dbcfa054393b16b7817eec1ec5fb8caf",
 "icons/Icon-maskable-512.png": "1ba73877555e0e0ae87d426a25a27552",
 "index.html": "c494e76216197ed1ee22c1573c4b0699",
 "/": "c494e76216197ed1ee22c1573c4b0699",
-"main.dart.js": "d968b5e6000e3ae2d2a426b3ef8cc56a",
+"main.dart.js": "e47ae56214f09c4ffa5c0ee26e92bb89",
 "manifest.json": "4a2198c0ba6cab70502dac0a601d9f39",
 "version.json": "699673d9149f2031ce9019297deb7ca3"};
 // The application shell files that are downloaded before a service worker can
