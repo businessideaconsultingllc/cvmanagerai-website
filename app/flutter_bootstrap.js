f63c18db7f105a4d78d805656c56f191a5e65c7e
Deployment Script <deploy@local.script>
2026-03-26 16:25:33 +0100

    Feat: Full SaaS SEO & UI Upgrade - Programmatic SEO, Blog Engine, and ATS Checker Tool

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4195809077"
+    serviceWorkerVersion: "3372786018"
   }
 });
