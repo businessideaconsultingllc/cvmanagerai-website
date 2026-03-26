d9e3fbcf3189c00b62bc4ced90d07f89fee85a1a
Deployment Script <deploy@local.script>
2026-03-26 21:28:16 +0100

    Fix: Restore consistent btn-nav purple button in navbar across all pages

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "198286501"
+    serviceWorkerVersion: "4117696268"
   }
 });
