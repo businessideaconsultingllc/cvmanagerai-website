c1c130295f4396ae95814b8483a7c1cf2b809213
Deployment Script <deploy@local.script>
2026-03-31 16:12:57 +0200

    fix: change navbar main CTA text from Free CV to Create Free CV

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2045659671"
+    serviceWorkerVersion: "2746682209"
   }
 });
