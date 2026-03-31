ea51dddc5987c4d555366db6c92153fac6628f00
Deployment Script <deploy@local.script>
2026-03-31 14:44:43 +0200

    fix: contrast updates for header/footer and compare builders background

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "456016"
+    serviceWorkerVersion: "1870445523"
   }
 });
