f2693a5f31f7dd8940390631d39c78be070d1cce
Deployment Script <deploy@local.script>
2026-03-31 13:56:15 +0200

    feat: unify all colors - dark navy footer + CTA banners match header on all pages

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1963676013"
+    serviceWorkerVersion: "678505817"
   }
 });
