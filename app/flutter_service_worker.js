da047ad6db7b93e3e262c8b58524d53f0ab35261
Deployment Script <deploy@local.script>
2026-08-10 19:25:49 +0200

    set suitable name for each cv and cover letter

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "80492066c03b4d758091fa05d8f91f07
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "f4ad63eae138bd29996d91317eb50cfc",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "d0dd7fb7e024793a49060d22ce7e4fa2",
+"flutter_bootstrap.js": "03c523fb2b8e3c29bdebf2d5d399b544",
 "icons/Icon-192.png": "dbcfa054393b16b7817eec1ec5fb8caf",
 "icons/Icon-512.png": "1ba73877555e0e0ae87d426a25a27552",
 "icons/Icon-maskable-192.png": "dbcfa054393b16b7817eec1ec5fb8caf",
 "icons/Icon-maskable-512.png": "1ba73877555e0e0ae87d426a25a27552",
 "index.html": "4b2ea91dd5e30c5631c0c0d2443d3c02",
 "/": "4b2ea91dd5e30c5631c0c0d2443d3c02",
-"main.dart.js": "28de87e6990da71c800a308ad2d554c8",
+"main.dart.js": "0c08b839d2f1c805d30c1d68758bbe31",
 "manifest.json": "4a2198c0ba6cab70502dac0a601d9f39",
 "version.json": "699673d9149f2031ce9019297deb7ca3"};
 // The application shell files that are downloaded before a service worker can
