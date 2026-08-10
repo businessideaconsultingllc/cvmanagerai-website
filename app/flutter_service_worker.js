42696e5c115f8569558fec00d744c91e405ac6d8
Deployment Script <deploy@local.script>
2026-08-10 12:11:31 +0200

    continue change colours and branding for rest pages

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "80492066c03b4d758091fa05d8f91f07
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "f4ad63eae138bd29996d91317eb50cfc",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "1b31fa5e77c0c42bd141802322764688",
+"flutter_bootstrap.js": "c59f1ec60df6cab3a51e4deb6b57da85",
 "icons/Icon-192.png": "dbcfa054393b16b7817eec1ec5fb8caf",
 "icons/Icon-512.png": "1ba73877555e0e0ae87d426a25a27552",
 "icons/Icon-maskable-192.png": "dbcfa054393b16b7817eec1ec5fb8caf",
